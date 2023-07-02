62bc8eaf1060b18f87ae53c3ffc7c12c5234d55f
Boris Souquière <souquiereb1998@gmail.com>
2023-07-02 22:57:26 +0200

    scroll

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,14 +1,22 @@
 <script>
 import TechnoCard from '../components/TechnoCard.vue';
 export default {
-  components: {TechnoCard}
+  components: {TechnoCard},
+  methods:{
+    scrollTo(id){
+      console.log(id);
+      let elementToScroll = document.getElementById(id);
+      elementToScroll.scrollIntoView({behavior:'instant', block:'start'});
+    },
+
+  }
 }
 </script>
 <template>
   <main class="xl:grid relative">
       <div class="hidden try left-0 xl:w-1/5 xl:flex flex-col">
-        <a href="#" class="p-[20px] hover:bg-dark hover:text-mint "> Présentation </a>
-        <a href="#" class="p-[20px] hover:bg-dark hover:text-mint "> Technologies </a>
+        <a href="#" class="p-[20px] hover:bg-dark hover:text-mint " @click.prevent="scrollTo('pres')"> Présentation </a>
+        <a href="#" class="p-[20px] hover:bg-dark hover:text-mint " @click.prevent="scrollTo('tech')"> Technologies </a>
         <a href="#" class="p-[20px] hover:bg-dark hover:text-mint "> Soft Skils & Centres intérêts </a>
         <a href="#" class="p-[20px] hover:bg-dark hover:text-mint "> Projets </a>
         <a href="#" class="p-[20px] hover:bg-dark hover:text-mint "> Parcours </a>
@@ -16,8 +24,8 @@ export default {
       <div class="col-span-4 xl:page xl:absolute right-0 xl:w-4/5">
       <section class="bg-bottom p-[20px] bg-no-repeat bg-dark ">
         <article class="flex flex-col justify-center align-baseline max-w-[600px] m-auto">
-          <div class="p-[20px]">
-            <img src="../assets/img/moi.jpg" alt="" class=" w-[200px] rounded-md shadow-xl m-auto">
+          <div class="p-[20px]" id="pres">
+            <img src="../assets/img/moi.jpg" alt="" class=" w-[200px] rounded-md shadow-xl m-auto" id="pres">
           </div>
           <div class=" items-center flex flex-col text-white">
             <h1 class="p-[20px] text-center"> Présentation</h1>
@@ -28,7 +36,7 @@ export default {
         </article>
       </section>
       <section>
-        <h1 class="p-[20px] text-center"> Technologies </h1>
+        <h1 class="p-[20px] text-center" id="tech"> Technologies </h1>
         <article class="p-[20px] flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3  gap-[20px] m-auto items-center place-items-center max-w-[1440px]">
           <TechnoCard :imag="'html.png'" :name='"HTML"' :rank="3" />
           <TechnoCard :imag="'html.png'" :name='"CSS"' :rank="5" />
